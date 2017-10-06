f7010fa9fb76fa948ed77d5078c9d86dc40bf140
Felipe Alfaro <felialois@gmail.com>
2017-10-06 12:56:39 +0200

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) year Singular Me LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ * Written by felipe
+ */
+
+public class Main {
+
+  public static void main(String[] args){
+
+  }
+
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) year Singular Me LLC - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ * Written by felipe
+ */
+
+import junit.framework.TestCase;
+
+public class MainTest extends TestCase {
+  public void testMain() throws Exception {
+  }
+
+}
