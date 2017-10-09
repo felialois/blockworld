2a112afe5f9b8c8578c106bd6937ffcf1f16c244
Felipe Alfaro <felialois@gmail.com>
2017-10-09 15:00:37 +0200

    Update pom

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,10 +5,27 @@
  * Written by felipe
  */
 
+import java.util.ArrayList;
+import java.util.List;
+import structures.Block;
+import structures.State;
+
 public class Main {
 
   public static void main(String[] args){
 
+    int maxColumns = 3;
+    List<Block> blocks = new ArrayList<>();
+    State initialState = null;
+    State finalState = null;
+
+
+
+
+  }
+
+  private List<State> getPossibleStates(List<State> currentLevelStates){
+    return null;
   }
 
 }
